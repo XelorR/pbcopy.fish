eb0a1709575b5c89e9f7746bea2565ff2c8faf1f
Petr <petr@duck.com>
2024-01-21 20:34:39 +0300

    fix - use open fish func to handle other cases

--- functions/open.fish
+++ functions/open.fish
@@ -5,5 +5,7 @@ function open
             # fish default command handles well Macos and GNU/Linux
             # so we need to handle Termux (Android) only
             # https://fishshell.com/docs/current/cmds/open.html
+        case '*'
+            open $argv
     end
 end
