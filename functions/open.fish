ee2de280e58b36a4e69bad25b1a17db1e888d96f
Petr <petr@duck.com>
2024-01-21 20:32:21 +0300

    open command -- removed linux case because it is already handled by native fish open command

--- functions/open.fish
+++ functions/open.fish
@@ -1,10 +1,9 @@
 function open
     switch (uname -o)
-        case GNU/Linux
-            xdg-open $argv
         case Android
             termux-open $argv
-        case Darwin
-            open $argv
+            # fish default command handles well Macos and GNU/Linux
+            # so we need to handle Termux (Android) only
+            # https://fishshell.com/docs/current/cmds/open.html
     end
 end
